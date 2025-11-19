81e0de62697236c550915b77294348bc9197cb5c
BertAtibagos <atibagos.robert2300@gmail.com>
2025-11-19 16:40:00 +0800

    Some changes: - Migrated chart builders to index-component.js - Added new function: fetchDeptTotal and genReport

--- view/index-function.js
+++ view/index-function.js
@@ -13,7 +13,7 @@ async function totalStats(){
         document.getElementById("verified").textContent = result.verified;
         document.getElementById("unverified").textContent = result.unverified;
         document.getElementById("total").textContent = result.total_rec;
-        dnutChartBuilder(result);
+        // dnutChartBuilder(result);
     }
     catch(error){
         console.log("ERROR: ", error)
@@ -58,122 +58,41 @@ async function fetchPerCutOffTotal(){
     }
 }
 
-function dnutChartBuilder(result){
-
-    let valueNames = ["Verified","Unverified"];
-    let valueData = [result.verified, result.unverified];
-    let valueColors = ["#032a74", "#ffd700"];
-
-    new Chart("totalChart", {
-        type: "doughnut",
-        data: {
-            labels: valueNames,
-            datasets: [{
-                backgroundColor: valueColors,
-                data: valueData
-            }]
-        },
-        options:{
-            plugins: {
-                title: {
-                    display: true,
-                    text: "TADI Records Summary"
-                },
-                legend: {
-                    display: true
-                }
-            }
-        }
-    });
+async function fetchDeptTotal(){
+    try{
+        const request = await fetch(`controller/index-post.php`,{
+            method: "POST",
+            headers: {"Content-Type": "application/x-www-form-urlencoded"},
+            body: new URLSearchParams({
+                type: "GET_ALL_PROG_TOTAL"
+            })
+        });
+        const result = await request.json();
+        vertBarChartPerDeptBuilder(result);
+    }
+    catch(error){
+        console.log("ERROR: ", error)
+    }
 }
 
-function barChartMonthlyBuilder(result){
-    
-    let monthNames = [];
-    let verifiedData = [];
-    let unverifiedData = [];
+document.getElementById('generateBtn').addEventListener("click", (e)=>{
+    genReport();
+});
 
-    if(Array.isArray(result)){
-        result.forEach(data =>{
-            monthNames.push(data.month_name);
-            verifiedData.push(data.verified);
-            unverifiedData.push(data.unverified);
+async function genReport(){
+    try{
+        const request = await fetch(`controller/index-post.php`, {
+            method: "POST",
+            headers: {"Content-Type": "application/x-www-form-urlencoded"},
+            body: new URLSearchParams({
+                type: "GET_TADI_DETAILS_BY_CUTOFF"
+            })
         });
+
+        const result = await request.json();
+        console.log(result);
     }
-
-    new Chart("monthlyTotalChart", {
-        type: "bar",
-        data: {
-            labels: monthNames,
-            datasets: [
-                {
-                    label: "Verified",
-                    backgroundColor: "#032a74",
-                    data: verifiedData
-                },
-                {
-                    label: "Unverified",
-                    backgroundColor: "#ffd700",
-                    data: unverifiedData
-                }
-            ]
-        },
-        options:{
-            plugins: {
-                legend: {
-                    display: true
-                },
-                title: {
-                    display: true,
-                    text: "Monthly TADI Records"
-                }
-            }
-        }
-    });
-}
-
-function barChartPerCutBuilder(result){
-    
-    let periodNames = [];
-    let verifiedData = [];
-    let unverifiedData = [];
-
-    if(Array.isArray(result)){
-        result.forEach(data =>{
-            periodNames.push(data.cutoff_period);
-            verifiedData.push(data.verified);
-            unverifiedData.push(data.unverified);
-        });
+    catch(error){
+         console.log("ERROR: ", error);
     }
-
-    new Chart("perCutOffChart", {
-        type: "bar",
-        data: {
-            labels: periodNames,
-            datasets: [
-                {
-                    label: "Verified",
-                    backgroundColor: "#032a74",
-                    data: verifiedData
-                },
-                {
-                    label: "Unverified",
-                    backgroundColor: "#ffd700",
-                    data: unverifiedData
-                }
-            ]
-        },
-        options:{
-            responsive: true,
-            plugins: {
-                legend: {
-                    display: true
-                },
-                title: {
-                    display: true,
-                    text: "Per Cut-off TADI Records"
-                }
-            }
-        }
-    });
 }
