3ccdf5f71c1e901e2e67b3957e0161cf65d5fb76
BertAtibagos <atibagos.robert2300@gmail.com>
2025-11-19 16:33:10 +0800

    Some changes: - rearranged the chart cards - add new js file

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <link rel="stylesheet" type="text/css" href="css/hr.css">
 </head>
 <body>
+
     <div class="card-filter mb-3 p-3">
         <div class="row g-3 align-items-end">
 
@@ -48,12 +49,13 @@
             </div>
 
             <div class="col-md text-end">
-                <button id="generateBtn" class="btn px-4 shadow text-white gen-rep">
+                <button id="generateBtn" class="btn px-4 shadow btn-secondary text-white gen-rep">
                     Generate Report
                 </button>   
             </div>
         </div>
     </div>
+
     <div class="card mx-auto p-3 report-view">
         <div class="row">
             <div class="card col-md m-3 shadow stats verified">
@@ -71,22 +73,28 @@
         </div>
 
         <div class="row">
-            <div class="card col border shadow p-3 m-1 chart-container">
-                <canvas id="totalChart"></canvas>
-            </div>
-            <div class="card col border shadow p-3 m-1 chart-container">
+            <div class="card col border shadow p-3 m-1 chart-container2">
                 <canvas id="monthlyTotalChart"></canvas>
             </div>
+            <div class="card col border shadow p-3 m-1 chart-container2">
+                <canvas id="vertPerDeptChart"></canvas>
+            </div>
         </div>
+
         <div class="row">
-            <div class="card col border shadow p-3 m-1 chart-container">
+            <!-- <div class="card col border shadow p-3 m-1 chart-container1">
+                <canvas id="totalChart"></canvas>
+            </div> -->
+            <div class="card col border shadow p-3 m-1 chart-container2">
                 <canvas id="perCutOffChart"></canvas>
             </div>
         </div>
     </div>
+
 </body>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@4.5.1/dist/chart.umd.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script src="view/index-function.js?t=<?php echo time(); ?>"></script>
 <script src="view/index-script.js?t=<?php echo time(); ?>"></script>
+<script src="view/index-component.js?t=<?php echo time(); ?>"></script>
 
