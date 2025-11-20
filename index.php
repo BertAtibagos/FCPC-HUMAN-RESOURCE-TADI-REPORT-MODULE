0f311bb3353e9b6f2c3a4ea26d977731cc9662a7
BertAtibagos <atibagos.robert2300@gmail.com>
2025-11-20 16:29:24 +0800

    Added id to certain elements and add defer attribute to script tag

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
             </div>
 
             <div class="col-md name-search hide">
-                <input type="text" class="form-control shadow" placeholder="Name">
+                <input type="text" class="form-control shadow" placeholder="Name" id="nameSearch">
             </div>
 
             <div class="col-md dept-select hide">
@@ -56,7 +56,7 @@
         </div>
     </div>
 
-    <div class="card mx-auto p-3 report-view">
+    <div class="card mx-auto p-3 report-view" id="reportView">
         <div class="row">
             <div class="card col-md m-3 shadow stats verified">
                 <h6>Total Verified</h6>
@@ -94,7 +94,6 @@
 </body>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@4.5.1/dist/chart.umd.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-<script src="view/index-function.js?t=<?php echo time(); ?>"></script>
-<script src="view/index-script.js?t=<?php echo time(); ?>"></script>
-<script src="view/index-component.js?t=<?php echo time(); ?>"></script>
-
+<script src="view/index-function.js?t=<?php echo time(); ?>" defer></script>
+<script src="view/index-script.js?t=<?php echo time(); ?>" defer></script>
+<script src="view/index-component.js?t=<?php echo time(); ?>" defer></script>
